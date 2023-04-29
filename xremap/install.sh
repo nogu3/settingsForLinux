b02d2de25a3db599a44534d8a27d84cf3f3fa838
shoya-noguchi <nogukids@gmail.com>
2023-04-29 22:23:53 +0900

    binary download github

--- xremap/install.sh
+++ xremap/install.sh
@@ -1,5 +1,10 @@
+# download binary
+wget https://github.com/k0kubun/xremap/releases/download/v0.8.5/xremap-linux-x86_64-gnome.zip
+unzip xremap-linux-x86_64-gnome.zip
+rm xremap-linux-x86_64-gnome.zip
+
 # deploy
-cp -p xremap /usr/local/bin/
+mv xremap /usr/local/bin/
 mkdir -p /usr/local/etc/xremap
 cp -p config.yml /usr/local/etc/xremap/
 
@@ -24,3 +29,6 @@ systemctl enable xremap
 
 # start service
 systemctl start xremap
+
+# confirm service status
+systemctl status xremap
