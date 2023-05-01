23e7ddbadb958cabbc373c45719127b406323483
shoya-noguchi <nogukids@gmail.com>
2023-05-01 12:44:36 +0900

    fix install fingerprint

--- fingerprint/install.sh
+++ fingerprint/install.sh
@@ -1,7 +1,7 @@
 # FYI
 # https://zenn.dev/nemuki/articles/fd159fa428944a6a4e80
 
-apt remove fprintd
+apt remove -y fprintd
 add-apt-repository ppa:uunicorn/open-fprintd
 apt-get update
 apt install -y open-fprintd fprintd-clients python3-validity
